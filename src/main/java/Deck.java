b2eaf7400b0304fe9800475e5653631e3d1f2ecf
Graham-Stein <graham@guavoaviation.co.uk>
2018-11-22 14:51:18 +0000

    add method to populate deck

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> deckOfCards;
+
+    public Deck() {
+        this.deckOfCards = new ArrayList<>();
+    }
+
+    public ArrayList<Card> getDeckOfCards() {
+        return this.deckOfCards;
+    }
+
+    public Card getCard(SuitType suit, RankType rank) {
+        return new Card(suit, rank) ;
+    }
+
+    public void addCard(SuitType suit, RankType rank) {
+        this.deckOfCards.add(getCard(suit, rank));
+    }
+
+    public void populateDeck() {
+        for (SuitType suit : SuitType.values()) {
+            for (RankType rank : RankType.values()) {
+                addCard(suit, rank);
+            }
+        }
+    }
+
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    Deck deck;
+
+    @Before
+    public void setup() {
+        deck = new Deck();
+    }
+
+    @Test
+    public void hasEmptyDeckOfCards() {
+        assertEquals(0, deck.getDeckOfCards().size());
+    }
+
+    @Test
+    public void canAddACardToDeck() {
+        deck.addCard(SuitType.HEARTS, RankType.JACK);
+        assertEquals(1, deck.getDeckOfCards().size());
+    }
+
+    @Test
+    public void canPopulateDeck() {
+        deck.populateDeck();
+        assertEquals(52, deck.getDeckOfCards().size());
+    }
 }
